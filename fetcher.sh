da42c855736d9cbfb98c0adb29d9f9e8ebb406da
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2019-03-17 04:19:24 +0100

    fetcher pro dalsi roky

--- fetcher.sh
+++ fetcher.sh
@@ -1,12 +1,20 @@
-wget -O profile.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488"
+#!/bin/bash
+
+PROFILE_ID="5971ca164e7b8d5ea27f0488"
+
+wget -O profile.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID"
 wget -O item_names.json "https://www.cityvizor.cz/exports/v1/codelists/item-names"
 wget -O paragraph_names.json "https://www.cityvizor.cz/exports/v1/codelists/paragraph-names"
-wget -O budgets.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/budgets"
-wget -O 2018/budgets.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/budgets/2018"
-wget -O 2018/events.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/events/2018"
-wget -O 2018/payments.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/payments/2018"
-wget -O 2017/budgets.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/budgets/2017"
-wget -O 2017/events.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/events/2017"
-wget -O 2017/payments.json "https://www.cityvizor.cz/exports/v1/profiles/5971ca164e7b8d5ea27f0488/payments/2017"
+wget -O budgets.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/budgets"
+
+current_year=`date +"%Y"`
+for YEAR in $(seq 2017 $current_year)
+do
+  mkdir $YEAR
+  wget -O $YEAR/budgets.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/budgets/$YEAR"
+  wget -O $YEAR/events.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/events/$YEAR"
+  wget -O $YEAR/payments.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/payments/$YEAR"
+done
+
 git add .
 git commit -am "Fetched"
