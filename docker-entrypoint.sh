1a6b72877a04f1ce69b26562c005ad03ee4f2a25
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2019-06-05 03:33:09 +0200

    fixed commiting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,11 @@ git config --global user.name "LepsiPraha7 import bot"
 cd /source &&
 echo "Fetching from github.com..." &&
 git pull origin source &&
-cd .. &&
+git status
 echo "Fetching from praha7.cz..." &&
-./fetcher.sh &&
+/fetcher.sh &&
 echo "Pushing to github.com..." &&
-cd /source &&
-git add -A &&
+git add **/*.json &&
 git diff --quiet &&
 git diff --staged --quiet ||
 (git commit -am 'Auto imported new data' && git push $REPO source)
