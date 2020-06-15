ef790f688d60c8dfa6e3872999a1e965fcaa163a
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2020-06-15 02:54:56 +0200

    Přechod na nové API (od 29.1.2020)

--- fetcher.sh
+++ fetcher.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-PROFILE_ID="5971ca164e7b8d5ea27f0488"
+PROFILE_ID="4"
 
-wget -O profile.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID"
-wget -O item_names.json "https://www.cityvizor.cz/exports/v1/codelists/item-names"
-wget -O paragraph_names.json "https://www.cityvizor.cz/exports/v1/codelists/paragraph-names"
-wget -O budgets.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/budgets"
+wget -O years.json "https://www.cityvizor.cz/api/exports/profiles/$PROFILE_ID/years"
 
 current_year=`date +"%Y"`
 for YEAR in $(seq 2017 $current_year)
 do
   mkdir $YEAR
-  wget -O $YEAR/budgets.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/budgets/$YEAR"
-  wget -O $YEAR/events.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/events/$YEAR"
-  wget -O $YEAR/payments.json "https://www.cityvizor.cz/exports/v1/profiles/$PROFILE_ID/payments/$YEAR"
+  wget -O $YEAR/accounting.json "https://www.cityvizor.cz/api/exports/profiles/$PROFILE_ID/accounting/$YEAR"
+  wget -O $YEAR/events.json "https://www.cityvizor.cz/api/exports/profiles/$PROFILE_ID/events/$YEAR"
+  wget -O $YEAR/payments.json "https://www.cityvizor.cz/api/exports/profiles/$PROFILE_ID/payments/$YEAR"
 done
